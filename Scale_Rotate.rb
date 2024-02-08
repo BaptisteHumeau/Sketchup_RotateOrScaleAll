92dbbf6575049d4c90d0948dd982e59380f7586c
Baptiste Humeau <baptistephumeau@gmail.com>
2024-02-08 00:01:01 -0500

    Update UI box and message

--- Scale_Rotate.rb
+++ Scale_Rotate.rb
@@ -1,36 +1,37 @@
-# This script Scales or Rotates multiple sketchup components about their respective centers
 
 require "sketchup.rb"
 
 def scale_rotate()
-    choice = UI.messagebox("Choose an operation: Yes = Scale, No = Rotate", MB_YESNO, "Choose an operation") do |msg|
-        msg.buttons = [ "Scale", "Rotate" ]
-    end
-    
-    case choice
-    when IDYES  # Scale
+    operation = UI.inputbox(["Enter 'S' for Scale or 'R' for Rotate"], ["S"], "Choose an operation")[0].upcase
+   
+    case operation
+    when 'S'  # Scale
         scale_factor = UI.inputbox(["Scale Factor"], [2.0], "Enter the scale factor")[0]
-    
+   
         # Scale the selected objects
         selection = Sketchup.active_model.selection
         selection.each do |entity|
-        center = entity.bounds.center
-        transformation = Geom::Transformation.scaling(center, scale_factor.to_f)
-        entity.transform!(transformation)
+            center = entity.bounds.center
+            transformation = Geom::Transformation.scaling(center, scale_factor.to_f)
+            entity.transform!(transformation)
         end
-    when IDNO   # Rotate
+    when 'R'   # Rotate
         rotation_angle = UI.inputbox(["Rotation Angle"], [45], "Enter the rotation angle")[0].to_f
-    
+   
         # Rotate the selected objects
         selection = Sketchup.active_model.selection
         selection.each do |entity|
-        center = entity.bounds.center
-        transformation = Geom::Transformation.rotation(center, [0,0,1], rotation_angle.degrees)
-        entity.transform!(transformation)
+            center = entity.bounds.center
+            transformation = Geom::Transformation.rotation(center, [0,0,1], rotation_angle.degrees)
+            entity.transform!(transformation)
         end
+    else
+        UI.messagebox("Invalid input. Please enter 'S' for Scale or 'R' for Rotate.")
     end
 end
 
-UI.menu("Extensions").add_item("Scale/Rotate Selection Individually") {
-  scale_rotate()
-}
+if UI.messagebox("Add the extension to the menu?", MB_YESNO) == IDYES
+    UI.menu("Extensions").add_item("Scale/Rotate Selection Individually") {
+        scale_rotate()
+    }
+end
